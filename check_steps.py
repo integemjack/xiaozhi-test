a5e56c8d213bab0543d0448bce67d97f693fafbe
Ireoo <s@ireoo.com>
2026-06-18 21:45:12 +0800

    chore: remove temp debug files

--- check_steps.py
+++
@@ -1,7 +0,0 @@
-import json, sys
-data = json.load(sys.stdin)
-for j in data['jobs']:
-    print(f"{j['name']}: {j['conclusion']}")
-    if 'Windows' in j['name']:
-        for s in j['steps']:
-            print(f"  {s['name']}: {s['conclusion']}")
